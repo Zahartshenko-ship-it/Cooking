bbc155becc43a25739986d98436a194a86d61e7a
ieelok <cheburashkaxnaruto@gmail.com>
2026-05-25 15:56:48 +0500

    подключение к публичной бд

--- db_init.py
+++ db_init.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine, text
 def get_engine():
     DB_USER = 'postgres'
     DB_PASSWORD = 'JohnnyCage29'
-    DB_HOST = '127.0.0.1'
+    DB_HOST = '159.194.211.35'
     DB_PORT = '5432'
-    DB_NAME = 'recipes'
+    DB_NAME = 'mydb'
     return create_engine(f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
